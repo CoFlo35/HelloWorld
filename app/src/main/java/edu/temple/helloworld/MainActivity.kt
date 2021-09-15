5c01e547bef99cf8588c6a89e322097ee5f761c2
CoFlo35 <43580764+CoFlo35@users.noreply.github.com>
2021-09-15 12:23:24 -0400

    Changed format of Constraint Layout to match specifications

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button).setOnClickListener({v, -> (v as Button).text = "You clicked the button"})
 
+        findViewById<Button>(R.id.button).setOnClickListener { v, ->
+            (v as Button).text = "${getString(R.string.my_textChange)}"
+
+        }
+        //findViewById<TextView>(R.id.textView).setOnClickListener{p -> (p as TextView).text = "${getString(R.string.my_textChange)}"}
     }
 }
